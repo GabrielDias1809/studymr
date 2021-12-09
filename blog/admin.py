1d0113cd8eaec707afc7c4b90704c79db34ae89b
Gabriel <diasbusiness18@gmail.com>
2021-12-09 07:31:12 -0300

    estilizando admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display    = ('title','slug','author','published','status')
+    list_filter = ('status','created','author','published')
+    date_hierarchy = 'published'
+    search_fields   = ('title','content') 
+    prepopulated_fields = {'slug':('title',)}
